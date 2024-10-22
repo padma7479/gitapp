d61b88884437fd8bab99caf8a86060062e21e5ee
padma7479 <dwadasivpadmavathi07@gmail.com>
2024-10-22 12:51:29 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,5 +1,5 @@
-Class Sample{
-public static void main(String [] args){
-System.out.println("Hello World");
-}
+class Sample{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
